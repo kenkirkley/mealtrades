96ec7e554f43fd31633f9ecc42e7167e7995dcf5
unknown <kenkirkley1330@gmail.com>
2019-11-25 23:04:22 -0600

    Delete requests

--- tempapp.js
+++ tempapp.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+//STATUS CODES: 201: Changed 204: Delete
 app.patch('/api/v1/tours:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -14,6 +15,18 @@ app.patch('/api/v1/tours:id', (req, res) => {
     data: {
       tour: '<Updated Tour Here...'
     }
+  });
+});
+app.delete('/api/v1/tours:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
