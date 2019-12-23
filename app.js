1c2f1a2f89fe5f73c0a339a6127a43f3a40bd177
unknown <kenkirkley1330@gmail.com>
2019-12-23 16:34:32 -0600

    Lesson 184

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,8 +12,14 @@ const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 /////////////////////////////
 // 1. MIDDLEWARE
@@ -55,9 +62,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // request time, test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -66,6 +70,8 @@ app.use((req, res, next) => {
 
 ////////////////////////////////
 // 2. Routes
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
